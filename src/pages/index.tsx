e3182185eb418966bdcdfa39da196d4a8880c8ec
Alfredo Gabriel <amgabriel1@hotmail.com>
2022-12-03 17:59:11 -0300

    update: create basic front

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,21 +4,189 @@ import { BasicGraph } from '../components/BasicGraph'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  var options = {
+    autoResize: true,
+    height: '100%',
+    width: '100%',
+    locale: 'pt-BR',
+    locales: "pt-br",
+    clickToUse: false
+  }
   return (
     <>
-      <div className="max-w-md mx-auto mt-32 bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-        <div className="md:flex">
-          <div className="md:flex-shrink">
-            <div className="h-48 w-full object-cover md:h-full md:w-48">
-              {BasicGraph()}
-            </div>
-          </div>
-          <div className="p-8">
-            <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Case study</div>
-            <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Finding customers for your new business</a>
-            <p className="mt-2 text-gray-500">Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.</p>
+      <div>
+
+        <div className="flex overflow-hidden bg-white xl:max-h-screen">
+
+          <div className="bg-gray-900 opacity-50 hidden fixed inset-0 z-10" id="sidebarBackdrop"></div>
+          <div id="main-content" className="h-full w-full bg-gray-50 relative overflow-y-auto ">
+            <main>
+              <div className="pt-6 px-4">
+                <div className="w-full grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-2 gap-4">
+                  <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8  2xl:col-span-1">
+                    <div id="main-chart" className="h-full w-full">
+                      {BasicGraph(options)}
+                    </div>
+                  </div>
+                  <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8 ">
+                    <div className="mb-4 flex items-center justify-between">
+                      <div>
+                        <h3 className="text-xl font-bold text-gray-900 mb-2">Funções disponiveis</h3>
+                      </div>
+                    </div>
+                    <div className="flex flex-col mt-8">
+                      <div className="overflow-x-auto rounded-lg">
+                        <div className="align-middle inline-block min-w-full">
+                          <div className="shadow max-h-[500px] overflow-y-scroll sm:rounded-lg">
+                            <table className="min-w-full divide-y divide-gray-200">
+                              <thead className="bg-gray-50">
+                                <tr>
+                                  <th scope="col" className="p-4 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+
+                                  </th>
+                                  <th scope="col" className="p-4 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Descrição
+                                  </th>
+                                </tr>
+                              </thead>
+                              <tbody className="bg-white">
+                                <tr>
+                                  <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-900">
+                                    <div className="form-check">
+                                      <input className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" id="flexCheckChecked" />
+                                    </div>
+                                  </td>
+                                  <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-500">
+                                    True -&gt; Matriz | False -&gt; Lista 
+                                  </td>
+                                </tr>
+                                <tr className="bg-gray-50">
+                                  <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-900">
+                                    <div className="form-check">
+                                      <input className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" id="flexCheckChecked" />
+                                    </div>
+                                  </td>
+                                  <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-500">
+                                    True -&gt; Digrafo | False -&gt; Não digrafo
+                                  </td>
+                                </tr>
+                                <tr>
+                                  <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-900">
+                                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                      Criar
+                                    </button>
+                                  </td>
+                                  <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-500">
+                                  Vertice | Aresta
+                                  </td>
+                                </tr>
+                                <tr className="bg-gray-50">
+                                  <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-900 rounded-lg rounded-left">
+                                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                      Deletar
+                                    </button>
+                                  </td>
+                                  <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-500">
+                                  Vertice | Aresta
+                                  </td>
+                                </tr>
+                                <tr>
+                                  <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-900">
+                                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                      Verificar Aresta
+                                    </button>
+                                  </td>
+                                  <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-500">
+                                    Verifica se existe uma determinada aresta.
+                                  </td>
+                                </tr>
+                                <tr className="bg-gray-50">
+                                  <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-900 rounded-lg rounded-left">
+                                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                      Lista de Adjacencia
+                                    </button>
+                                  </td>
+                                  <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-500">
+                                    Retorna uma lista de adjacencia a partir do grafo inserido.
+                                  </td>
+                                </tr>
+                                <tr>
+                                  <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-900">
+                                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                      Contador
+                                    </button>
+                                  </td>
+                                  <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-500">
+                                    Retorna a contagem de vertices e arestas.
+                                  </td>
+                                </tr>
+                                <tr className="bg-gray-50">
+                                  <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-900 rounded-lg rounded-left">
+                                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                      Verifica Grau
+                                    </button>
+                                  </td>
+                                  <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-500">
+                                    Retorna o grau de um vertice.
+                                  </td>
+                                </tr>
+                                <tr>
+                                  <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-900">
+                                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                      Busca em Profundidade.
+                                    </button>
+                                  </td>
+                                  <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-500">
+                                    Realiza a busca em profundidade.
+                                  </td>
+                                </tr>
+                                <tr className="bg-gray-50">
+                                  <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-900 rounded-lg rounded-left">
+                                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                      Busca em Largua.
+                                    </button>
+                                  </td>
+                                  <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-500">
+                                    Realiza a busca em largura no grafo dado.
+                                  </td>
+                                </tr>
+                                <tr>
+                                  <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-900">
+                                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                      AGM.
+                                    </button>
+                                  </td>
+                                  <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-500">
+                                    Retorna a arvore geradora minima segundo Prim.
+                                  </td>
+                                </tr>
+                                <tr className="bg-gray-50">
+                                  <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-900 rounded-lg rounded-left">
+                                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                      Dijkstra.
+                                    </button>
+                                  </td>
+                                  <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-500">
+                                    Retorna um grafo com os pesos relaxados.
+                                  </td>
+                                </tr>
+                              </tbody>
+                            </table>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </main>
+
+            <p className="text-center text-sm text-gray-500 my-10">
+              &copy; 2022-2022 <a href="https://github.com/alverad-katsuro/grafos-front" className="hover:underline" target="_blank">Alverad Katsuro</a>. All rights reserved.
+            </p>
           </div>
         </div>
+
       </div>
     </>
 
